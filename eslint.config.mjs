40c8d60e1847bf86cbe615cbf409b24c339a5c4b
Jose Sanchez <mrsanchez.dev@gmail.com>
2026-07-20 15:06:33 -0400

    last step: Update ESLint config to include 'references/**' in global ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,8 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    // Reference fixtures are not part of the Next app source.
+    "references/**",
   ]),
 ]);
 
